8418b84f8256ede61c166af56c07eb5b9ee49462
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2018-01-15 16:46:10 -1200

    Menginput string

--- 1.cpp
+++ 1.cpp
@@ -8,4 +8,16 @@ using namespace std;
 	};
 	int main(){
 	sepatu drkevin, vanz, eagle;
+	
+	drkevin.kualitas = "Kualitas sangat baik ";
+	vanz.kualitas = "Kualitas standar ";
+	eagle.kualitas = "Kualitas kurang baik ";
+	
+	drkevin.bahan = "Bahan dari kulit buaya ";
+	vanz.bahan = "Bahan dari kain  ";
+	eagle.bahan = "Bahan dari kulit sapi ";
+	
+	drkevin.ukuran = "Ukuran 40  - 42 ";
+	vanz.ukuran = "Ukuran 40  - 41  ";
+	eagle.ukuran = "Ukuran 38 - 40 ";
 };
